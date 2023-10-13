7f8f8130e2584a8b7e11711ae0591b3f58fd8a97
Jun <wnsghd1@naver.com>
2023-10-13 18:02:03 +0900

    Undo Button

--- js/sketchPad.js
+++ js/sketchPad.js
@@ -9,6 +9,13 @@ class SketchPad{
     `;
     container.appendChild(this.canvas);
 
+    const lineBreak = document.createElement("br");
+    container.appendChild(lineBreak);
+    
+    this.undoBtn = document.createElement("button");
+    this.undoBtn.innerHTML = "UNDO";
+    container.appendChild(this.undoBtn);
+    
     this.ctx = this.canvas.getContext("2d");
 
     this.paths = [];
