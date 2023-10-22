c713f76ad3593d5e6afb01d1732e927718a87737
Jun <wnsghd1@naver.com>
2023-10-22 22:36:58 +0900

    create samples.json

--- node/dataset_generator.js
+++ node/dataset_generator.js
@@ -6,3 +6,28 @@ constants.DATASET_DIR = constants.DATA_DIR + '/dataset';
 constants.JSON_DIR = constants.DATA_DIR + '/json';
 constants.IMG_DIR = constants.DATA_DIR + '/img';
 constants.SAMPLES = constants.DATA_DIR + '/samples.json';
+
+const fs = require('fs');
+
+const fileNames = fs.readdirSync(constants.RAW_DIR);
+const samples = [];
+let id = 1;
+fileNames.forEach(fn => {
+  const content = fs.readFileSync(
+    constants.RAW_DIR + '/' + fn
+    );
+
+  const {session, student, drawings} = JSON.parse(content);
+  for(let label in drawings) {
+    samples.push({
+      id,
+      label,
+      student_name: student,
+      student_id:session
+    });
+    id++;
+  }
+});
+
+fs.writeFileSync(constants.SAMPLES, JSON.stringify(samples));
+
