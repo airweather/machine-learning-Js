3dc8b353b30d7a86e548a1153b7a3e9d89ed659f
Jun <wnsghd1@naver.com>
2023-10-27 02:15:31 +0900

    utils, print progress

---
+++ common/utils.js
@@ -0,0 +1,12 @@
+const utils = {};
+
+utils.printProgress = (count, max) => {
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  const percent = utils.formatPercent(
+    count / max
+  );
+  process.stdout.write(
+    count + '/' + max + ' (' + percent + ')'
+  );
+}
